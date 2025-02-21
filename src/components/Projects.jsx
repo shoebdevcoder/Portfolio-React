fd30d6409ec631469c8770298b829892338a7e9b
shoeb siddiqui <shoebahmedsiddiqui2747@gmail.com>
2025-02-22 02:49:18 +0530

    project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const Projects = () => {
   return (
     <div className='Projects'>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure ab sit nam, optio similique autem vitae, minus deserunt reprehenderit, nulla pariatur totam enim eius fugit aut. Quod eum nihil hic.</p>
+      <div className="project_lists">
+        <a href="">Matrimony Website</a>
+      </div>
     </div>
   )
 }
