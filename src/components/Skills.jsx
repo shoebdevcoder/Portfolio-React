cc4df7100556b8eba52de2dad37ea8b4c78ab0e2
shoeb siddiqui <shoebahmedsiddiqui2747@gmail.com>
2025-05-06 15:32:34 +0530

    skills UI updated

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -18,41 +18,82 @@ import psm from "../assets/images/postman.svg";
 import npm from "../assets/images/icons8-npm-48.png";
 
 const Skills = () => {
+  // const TopSkills = [
+  //   {
+  //     title: "Top Skills",
+  //     items: [
+  //       { name: "React", img: react },
+  //       { name: "HTML", img: html },
+  //       { name: "CSS", img: css },
+  //       { name: "JavaScript", img: js },
+  //       { name: "Node.js", img: node },
+  //       { name: "Express.js", img: exp },
+  //       { name: "MongoDB", img: mdb },
+  //     ],
+  //   },
+  // ];
+
+  // const Frameworks = [
+  //   {
+  //     title: "Frameworks",
+  //     items: [
+  //       { name: "Redux", img: rdx },
+  //       { name: "Tailwind CSS", img: tld },
+  //       { name: "Vite", img: vite },
+  //       { name: "Bootstrap", img: bts },
+  //       { name: "SASS", img: sass },
+  //     ],
+  //   },
+  // ];
+
+  // const Tools = [
+  //   {
+  //     title: "Tools",
+  //     items: [
+  //       { name: "Git", img: git },
+  //       { name: "GitHub", img: github },
+  //       { name: "Postman", img: psm },
+  //       { name: "VS Code", img: vscode },
+  //       { name: "npm", img: npm },
+  //     ],
+  //   },
+  // ];
+
   return (
     <div className="Skills">
+      {/* <h4>Top Skills</h4>
       <div className="skill-origin">
-        <h4>Top Skills</h4>
-        <div className="skills-main">
+        <div className="skills-main item1">
           <img src={react} alt="" />
           <h5>React JS</h5>
         </div>
-        <div className="skills-main">
+        <div className="skills-main item2">
           <img src={html} alt="" />
           <h5>HTML</h5>
         </div>
-        <div className="skills-main">
+        <div className="skills-main item3">
           <img src={css} alt="" />
           <h5>CSS</h5>
         </div>
-        <div className="skills-main">
+        <div className="skills-main item4">
           <img src={js} alt="" />
           <h5>JavaScript</h5>
         </div>
-        <div className="skills-main">
+        <div className="skills-main item5">
           <img src={node} alt="" />
           <h5>Node JS</h5>
         </div>
-        <div className="skills-main">
+        <div className="skills-main item6">
           <img src={exp} alt="" />
           <h5>Express JS</h5>
         </div>
-        <div className="skills-main">
+        <div className="skills-main item7">
           <img src={mdb} alt="" />
           <h5>Mongo DB</h5>
         </div>
       </div>
+      <h4>Frameworks</h4>
       <div className="skill-origin">
-        <h4>Frameworks</h4>
         <div className="skills-main">
           <img src={bts} alt="" />
           <h5>Bootstrap</h5>
@@ -74,8 +115,8 @@ const Skills = () => {
           <h5>Sass</h5>
         </div>
       </div>
+      <h4>Tools</h4>
       <div className="skill-origin">
-        <h4>Tools</h4>
         <div className="skills-main">
           <img src={git} alt="" />
           <h5>git</h5>
@@ -96,7 +137,113 @@ const Skills = () => {
           <img src={npm} alt="" />
           <h5>NPM</h5>
         </div>
+      </div> */}
+
+      {/* Sliding Skills */}
+
+      {/* <div className="parent-skill">
+        <div className="child-skill"></div>
+      </div> */}
+
+      {/* <div className="skill_one">
+        {TopSkills.map((section, index) => (
+          <>
+            <h2 className="text-primary">{section.title}</h2>
+            <div className="items" key={index}>
+              {section.items.map((item, i) => (
+                <div key={i} className="itm">
+                  <img src={item.img} alt="" />
+                </div>
+              ))}
+            </div>
+          </>
+        ))}
+      </div> */}
+
+      <div className="skill_one">
+      <h2 className="text-primary">TOP SKILLS</h2>
+        <div className="items">
+          <div className="itm itm1">
+            <img src={react} alt="" />
+          </div>
+          <div className="itm itm2">
+            <img src={html} alt="" />
+          </div>
+          <div className="itm itm3">
+            <img src={css} alt="" />
+          </div>
+          <div className="itm itm4">
+            <img src={js} alt="" />
+          </div>
+          <div className="itm itm5">
+            <img src={node} alt="" />
+          </div>
+          <div className="itm itm6">
+            <img src={exp} alt="" />
+          </div>
+          <div className="itm itm7">
+            <img src={mdb} alt="" />
+          </div>
+        </div>
       </div>
+
+      <div className="skill_two">
+        <h2 className="text-primary">FRAMEWORKS</h2>
+        <div className="items">
+          <div className="itm itm1">
+            <img src={rdx} alt="" />
+          </div>
+          <div className="itm itm2">
+            <img src={tld} alt="" />
+          </div>
+          <div className="itm itm3">
+            <img src={vite} alt="" />
+          </div>
+          <div className="itm itm4">
+            <img src={bts} alt="" />
+          </div>
+          <div className="itm itm5">
+            <img src={sass} alt="" />
+          </div>
+        </div>
+      </div>
+
+      <div className="skill_three">
+        <h2 className="text-primary">TOOLS</h2>
+        <div className="items">
+          <div className="itm itm1">
+            <img src={git} alt="" />
+          </div>
+          <div className="itm itm2">
+            <img src={github} alt="" />
+          </div>
+          <div className="itm itm3">
+            <img src={psm} alt="" />
+          </div>
+          <div className="itm itm4">
+            <img src={vscode} alt="" />
+          </div>
+          <div className="itm itm5">
+            <img src={npm} alt="" />
+          </div>
+        </div>
+      </div>
+
+      {/* {skills.map((section, index) => (
+        <div key={index} className="skill-section">
+          <h2 className="section-title">{section.title}</h2>
+          <div className="slider">
+            <div className="slide-track">
+              {section.items.concat(section.items).map((item, i) => (
+                <div className="slide" key={i}>
+                  <img src={item.img} alt={item.name} />
+                  <p>{item.name}</p>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      ))} */}
     </div>
   );
 };
